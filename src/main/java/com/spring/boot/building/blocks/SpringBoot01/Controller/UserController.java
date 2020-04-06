842f6fa8d7cb7d24223872a46d01c5bd10453b1d
Oshin Banerjee <oshinbanerjee8oct96@gmail.com>
2020-04-06 12:28:48 +0530

    Exception handling

--- src/main/java/com/spring/boot/building/blocks/SpringBoot01/Controller/UserController.java
+++ src/main/java/com/spring/boot/building/blocks/SpringBoot01/Controller/UserController.java
@@ -1,10 +1,13 @@
-package com.spring.boot.building.blocks.SpringBoot01.Controller;
+package com.otp.OtpEmailMobile.Controller;
 
 
-import com.spring.boot.building.blocks.SpringBoot01.Entity.User;
-import com.spring.boot.building.blocks.SpringBoot01.Service.UserService;
+import com.otp.OtpEmailMobile.Entity.User;
+import com.otp.OtpEmailMobile.Exception.UserNotFoundException;
+import com.otp.OtpEmailMobile.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,17 +28,20 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public Optional<User> getUserById(@PathVariable("id") long id){
-        return user.getUserById(id);
-
+        try {
+            return user.getUserById(id);
+        }catch (UserNotFoundException uex){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,uex.getMessage());
+        }
     }
     @PutMapping("/users/{id}")
     public User updateUserById(@RequestBody User u,@PathVariable("id")long id){
         return user.updateUserById(id,u);
     }
-    @DeleteMapping("/users/{id}")
-    public Optional<User> deleteUserById(@PathVariable("id")long id){
-        return user.deleteUserById(id);
-    }
+//    @DeleteMapping("/users/{id}")
+//    public Optional<User> deleteUserById(@PathVariable("id")long id){
+//        return user.deleteUserById(id);
+//    }
 
     @GetMapping("/users/username/{username}")
     public User getUserByUserName(@PathVariable("username") String username){
