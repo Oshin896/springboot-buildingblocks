90e8eaeb039a4db1c8adab2039b76c45614a1f29
Oshin Banerjee <oshinbanerjee8oct96@gmail.com>
2020-04-10 12:40:19 +0530

    First Commit- One to many

--- src/main/java/com/spring/boot/building/blocks/SpringBoot01/Controller/OrderController.java
+++ src/main/java/com/spring/boot/building/blocks/SpringBoot01/Controller/OrderController.java
@@ -8,7 +8,9 @@ import com.spring.boot.building.blocks.SpringBoot01.Exception.UserNotFoundExcept
 import com.spring.boot.building.blocks.SpringBoot01.Repository.OrderRepository;
 import com.spring.boot.building.blocks.SpringBoot01.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,19 +26,19 @@ public class OrderController {
     private OrderRepository orderRepository;
 
     @GetMapping("/{userid}/orders")
-    public List<Order> getAllOrders(@PathVariable Long userid) throws UserNotFoundException {
+    public List<Order> getAllOrders(@PathVariable Long userid)  {
         Optional<User> userOptional=userRepository.findById(userid);
         if(!userOptional.isPresent()){
-            throw new UserNotFoundException("User Not Found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
         }
         return userOptional.get().getOrders();
     }
 
     @PostMapping("/{userid}/orders")
-    public void createOrder(@PathVariable Long userid, @RequestBody Order order) throws UserNotFoundException {
+    public void createOrder(@PathVariable Long userid, @RequestBody Order order) {
         Optional<User> userOptional=userRepository.findById(userid);
         if(!userOptional.isPresent()){
-            throw new UserNotFoundException("User Not Found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
 
         }
         User user= userOptional.get();
@@ -45,19 +47,19 @@ public class OrderController {
     }
 
     @GetMapping("/{userid}/orders/{orderid}")
-    public Order getOrderByOrderId(@PathVariable("userid") Long userid, @PathVariable("orderid") Long orderid) throws UserNotFoundException, OrderNotFoundException {
+    public Order getOrderByOrderId(@PathVariable("userid") Long userid, @PathVariable("orderid") Long orderid){
         Optional<User> userOptional=userRepository.findById(userid);
         if(!userOptional.isPresent()){
-            throw new UserNotFoundException("User Not Found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
         }
         Optional<Order> orderOptional=orderRepository.findById(orderid);
         if(!orderOptional.isPresent()){
-            throw new OrderNotFoundException("Order not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Order not found");
         }
 
         Optional<Order> orderPresent=orderRepository.findOrderByOrderidAndUser(orderid,userOptional.get());
         if(!orderPresent.isPresent())
-            throw  new OrderNotFoundException("Order with orderid:"+orderid+" is not found with user:"+userid);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Order with orderid:"+orderid+" is not found with user:"+userid);
 
         return orderPresent.get();
     }
