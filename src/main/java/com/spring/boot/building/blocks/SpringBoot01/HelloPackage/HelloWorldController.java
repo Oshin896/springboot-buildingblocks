2cc7a0943caac90746c958c5c919990b4a2d608b
Oshin Banerjee <oshinbanerjee8oct96@gmail.com>
2020-04-26 11:15:44 +0530

    First Commit - Internationalization

--- src/main/java/com/spring/boot/building/blocks/SpringBoot01/HelloPackage/HelloWorldController.java
+++ src/main/java/com/spring/boot/building/blocks/SpringBoot01/HelloPackage/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.spring.boot.building.blocks.SpringBoot01.HelloPackage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 @GetMapping("/hello")
     public String helloWorld(){
         return "Hello-World1 ";
@@ -12,5 +20,13 @@ public class HelloWorldController {
 @GetMapping("/helloBean")
     public UserDetail helloWorldBean(){
     return new UserDetail("Oshin","Banerjee","Jamshedpur");
+    }
+@GetMapping("/hello-int")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language",required = false) String locale){
+    return messageSource.getMessage("label.hello",null,new Locale(locale));
+}
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18NFormat(){
+        return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
     }
 }
--- src/main/java/com/spring/boot/building/blocks/SpringBoot01/OtpCatcherApplication.java
+++ src/main/java/com/spring/boot/building/blocks/SpringBoot01/OtpCatcherApplication.java
@@ -2,7 +2,12 @@ package com.spring.boot.building.blocks.SpringBoot01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.Locale;
 
 
 @SpringBootApplication
@@ -12,4 +17,17 @@ public class OtpCatcherApplication {
 		SpringApplication.run(OtpCatcherApplication.class, args);
 	}
 
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver=new AcceptHeaderLocaleResolver();
+	localeResolver.setDefaultLocale(Locale.US);
+	return localeResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
